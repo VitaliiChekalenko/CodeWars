95b7461118e57b503c8b79ac65fdade90ea54e12
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-01-30 17:08:34 +0200

    ee

--- 7 kyu/SumofOddCubedNumbers/src/main/java/FindOddCubes.java
+++ 7 kyu/SumofOddCubedNumbers/src/main/java/FindOddCubes.java
@@ -4,13 +4,20 @@ public class FindOddCubes {
  int sum = 0;
  
  for (int i=0; i<arr.length; i++){
-   
-   if(arr[i]%2!=0){
+
      sum+= Math.pow(arr[i], 2);
+
    }
- 
- }
-  
+
    return sum;
   }
 }
+
+
+/* if(arr[i]<0 && Math.pow(arr[i], 2)%2!=0){
+     sum+= Math.pow(arr[i], 2)*(-1);
+           System.out.println(Math.pow(arr[i], 2)*(-1));
+       }else if(Math.pow(arr[i], 2)%2!=0){
+           sum+= Math.pow(arr[i], 2);
+           System.out.println(Math.pow(arr[i], 2));
+       }*/
