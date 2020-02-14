34df67f22d15d5dce6bc73312dd3eba4075b7937
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2020-02-14 17:10:32 +0200

    df

---
+++ 6 kyu/Give me a Diamond/src/main/java/Diamond.java
@@ -0,0 +1,40 @@
+/*Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+Task
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+Examples
+A size 3 diamond:
+
+ *
+***
+ *
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+...that is: " *\n ***\n*****\n ***\n *\n"
+
+
+https://www.codewars.com/kata/5503013e34137eeeaa001648/train/java
+
+*/
+
+class Diamond {
+    public static String print(int n) {
+        String s = " ";
+        System.out.println(s.repe);
+
+        for (int i = 0; i < n; i += 2) {
+          s+= ("*").repeate
+        }
+
+    }
+}
---
+++ 6 kyu/Give me a Diamond/src/test/java/DiamondTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class DiamondTest {
+    @Test
+    public void testDiamond3() {
+      StringBuffer expected = new StringBuffer();
+      expected.append(" *\n");
+      expected.append("***\n");
+      expected.append(" *\n");
+      
+      assertEquals(expected.toString(), Diamond.print(3));
+    }
+    
+    @Test
+    public void testDiamond5() {
+      StringBuffer expected = new StringBuffer();
+      expected.append("  *\n");
+      expected.append(" ***\n");
+      expected.append("*****\n");
+      expected.append(" ***\n");
+      expected.append("  *\n");
+      
+      assertEquals(expected.toString(), Diamond.print(5));
+    }  
+    
+    @Test
+    public void testDiamond1() {
+      StringBuffer expected = new StringBuffer();
+      expected.append("*\n");
+      assertEquals(expected.toString(), Diamond.print(1));
+    }  
+    
+    @Test
+    public void testDiamond0() {
+      assertEquals(null, Diamond.print(0));
+    } 
+    
+    @Test
+    public void testDiamondMinus2() {
+      assertEquals(null, Diamond.print(-2));
+    }
+    
+    @Test
+    public void testDiamond2() {
+      assertEquals(null, Diamond.print(2));
+    }    
+}
